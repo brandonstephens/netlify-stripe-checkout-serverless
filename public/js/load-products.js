ac6e1190d1678b64dc352090b08f1e3d43e46dc2
Brandon Stephens <brandonstephens@me.com>
2020-04-17 20:02:27 -0400

    yup

--- public/js/load-products.js
+++ public/js/load-products.js
@@ -1,3 +1,22 @@
+function createProductFromTemplate(item) {
+  const template = document.querySelector('#product')
+  const product = template.content.cloneNode(true)
+
+  product.querySelector('h2').innerText = item.name
+  product.querySelector('.description').innerText = item.description
+  product.querySelector('[name="sku"]').value = item.sku
+  product.querySelector('.price').value = Intl.NumberFormat('en-us', {
+    style: 'currency',
+    currency: item.currency,
+  }).format((item.amount / 100).toFixed(2))
+
+  const image = product.querySelector('img')
+  img.src = item.image
+  img.alt = item.name
+
+  return product
+}
+
 export async function loadProducts() {
   const data = await fetch('/.netlify/functions/get-products.json')
     .then((res) => res.json())
@@ -5,8 +24,8 @@ export async function loadProducts() {
 
   const products = document.querySelector('.products')
 
-  const pre = document.createElement('pre')
-  pre.innerText = JSON.stringify(data, null, 2)
-
-  products.appendChild(pre)
+  data.forEach((item) => {
+    const product = createProductFromTemplate(item)
+    products.appendChild(product)
+  })
 }
