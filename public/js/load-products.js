d16fa86e1f1f20f20a9a527ecbe182a9964758b4
Brandon Stephens <brandonstephens@me.com>
2020-04-17 21:13:11 -0400

    y

--- public/js/load-products.js
+++ public/js/load-products.js
@@ -17,7 +17,7 @@ function createProductFromTemplate(item) {
 }
 
 export async function loadProducts() {
-  const data = await fetch('/.netlify/functions/get-products.json')
+  const data = await fetch('/.netlify/functions/get-products')
     .then((res) => res.json())
     .catch((err) => console.error(err))
 
